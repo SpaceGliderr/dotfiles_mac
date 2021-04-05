7e44d2bff44c0749d5ea7d680c9ae75aced511cd
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2021-04-05 19:01:28 +0800

    Install Alfred and add no-quarantine flag to other installls

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -6,4 +6,5 @@ echo "\n <<<<< Starting Homebrew Setup >>>>>\n"
 
 brew install bat
 
-brew install visual-studio-code
+brew install --no-quarantine visual-studio-code
+brew install --no-quarantine alfred
